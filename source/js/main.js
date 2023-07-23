a92dab32c438c9e802447292b5f4e8dde71f75fa
restby <restby@gmail.com>
2023-07-24 00:50:48 +0300

    subs start

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
-
+import {initTabs} from './vendor/init-tabs';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -21,6 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
+    // initTabs();
   });
 });
 
